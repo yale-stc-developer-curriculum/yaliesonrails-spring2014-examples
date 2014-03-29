e3435195e527e3aa3cb4ba27c192ae9eedba0fc0
caseywatts <casey.s.watts@gmail.com>
2014-03-29 13:48:32 -0400

    findandreplace

---
+++ day6/5 findandreplace.js
@@ -0,0 +1,12 @@
+//explain regular expressions in general
+
+//document.body is the <body> tag
+//innerHTML is a way to select everything inside a node as text (good for find and replace)
+//replace uses a regular expression, like find-and-replace
+//.replace(/FIND/g, "REPLACE")
+//
+// /FIND/g
+// the /.../ part outlines the regex
+// the g means "global" - not just the first one but find and replace all
+
+document.body.innerHTML = document.body.innerHTML.replace(/target string/g, "replacement string");
